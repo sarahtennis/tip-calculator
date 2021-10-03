56ee51bce26aee2004552a219dd2efb9a690cfbf
Sarah Tennis <sarah.n.tennis@gmail.com>
2021-10-03 11:49:37 -0500

    Move Total class

--- main.js
+++ main.js
@@ -181,6 +181,44 @@ class People {
   }
 }
 
+class Total {
+  constructor (calculations, reset) {
+    this.calculations = calculations;
+    this.reset = reset;
+    this.tipPerPerson = document.querySelector('#tip-per-person');
+    this.totalPerPerson = document.querySelector('#total-per-person');
+    this.resetButton = document.querySelector('.reset-btn');
+    this.updateValues();
+    this.initResetEvents();
+  }
+
+  initResetEvents() {
+    if (!this.resetButton) {
+      return;
+    }
+    this.resetButton.addEventListener('click', (e) => {
+      this.reset();
+    });
+  }
+
+  setCalculations (newCalculations) {
+    this.calculations = { ...this.calculations, ...newCalculations };
+    this.updateValues();
+  }
+
+  updateValues () {
+    const { bill, tip, people } = this.calculations;
+    if (bill === null || tip === null || !people) {
+      this.tipPerPerson.innerText = Number(0).toFixed(2);
+      this.totalPerPerson.innerText = Number(0).toFixed(2);
+      return;
+    }
+    const tipTotal = (bill * tip / 100);
+    this.tipPerPerson.innerText = (tipTotal / people).toFixed(2);
+    this.totalPerPerson.innerText = ((bill + tipTotal) / people).toFixed(2);
+  }
+}
+
 class NumberInput {
   constructor (selector) {
     this.input = document.querySelector(selector);
@@ -224,44 +262,6 @@ class IntegerInput extends NumberInput {
 
   // No action on valid input
   defaultOnValidInput () {}
-}
-
-class Total {
-  constructor (calculations, reset) {
-    this.calculations = calculations;
-    this.reset = reset;
-    this.tipPerPerson = document.querySelector('#tip-per-person');
-    this.totalPerPerson = document.querySelector('#total-per-person');
-    this.resetButton = document.querySelector('.reset-btn');
-    this.updateValues();
-    this.initResetEvents();
-  }
-
-  initResetEvents() {
-    if (!this.resetButton) {
-      return;
-    }
-    this.resetButton.addEventListener('click', (e) => {
-      this.reset();
-    });
-  }
-
-  setCalculations (newCalculations) {
-    this.calculations = { ...this.calculations, ...newCalculations };
-    this.updateValues();
-  }
-
-  updateValues () {
-    const { bill, tip, people } = this.calculations;
-    if (bill === null || tip === null || !people) {
-      this.tipPerPerson.innerText = Number(0).toFixed(2);
-      this.totalPerPerson.innerText = Number(0).toFixed(2);
-      return;
-    }
-    const tipTotal = (bill * tip / 100);
-    this.tipPerPerson.innerText = (tipTotal / people).toFixed(2);
-    this.totalPerPerson.innerText = ((bill + tipTotal) / people).toFixed(2);
-  }
 }
 
 const calculator = new Calculator();
